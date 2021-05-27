8effc9f44e1432b6bbe9bb07606b938f6dd4e22a
fadiuwk <52964451+fadiuwk@users.noreply.github.com>
2021-05-27 08:55:16 +0200

    Add files via upload

---
+++ build/precache-manifest.95b55b904718dc5cbee914d132c46d12.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1356daca867dedbb1562fcede6cf25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80bdabe2522f2d53a78e",
+    "url": "/static/js/main.c373202f.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
